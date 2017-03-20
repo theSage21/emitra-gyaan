d24927879fd3ac5520081524f679516f9fd68e02
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-03-20 15:10:15 +0530

    1510 ====
    - remove scrap files
    - notebook update
    - start work on website

--- webserver.py
+++ webserver.py
@@ -1,9 +1,18 @@
 from bottle import Bottle, route, get, post, run
 
+def template(name):
+    with open('templates/'+name) as fl:
+        text = fl.read()
+    return text
+
 app = Bottle()
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+###############################
 
+@app.route('/')
+def home():
+    html = template('home.html')
+    return html
+
+###############################
 app.run(host='localhost', port=8080, debug=True)
