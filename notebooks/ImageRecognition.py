3cb5fb275fe2f4749f84fbea931d9595d0094e65
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-03-21 03:23:42 +0530

    classifier trainers

---
+++ notebooks/ImageRecognition.py
@@ -0,0 +1,140 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import seaborn as sns
+import bs4
+import requests
+import os
+from scipy.misc import imresize
+from bleedml.classifiers import CascadeForest
+from bleedml.utils import multi2Dscan, scan2D
+from sklearn.model_selection import cross_val_score
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from importlib import reload
+get_ipython().magic('pylab inline')
+
+
+# In[2]:
+
+def get_image_links(search, limit=100):
+    link = 'https://www.google.com/search?q={search}&oq={search}&start={start}'
+    start = 0
+    links = []
+    while len(links) < limit:
+        print(start)
+        page = requests.get(link.format(search=search, start=0))
+        if page.status_code == 200:
+            soup = bs4.BeautifulSoup(page.text, 'html5lib')
+            newlinks = [i.get('src') for i in soup.find_all('img')]
+            start += len(newlinks)
+            links.extend(newlinks)
+        else:
+            print('error in link')
+    return links
+
+
+# In[3]:
+
+if not os.path.exists('wheatlinks'):
+    links = get_image_links('wheat')
+    links = list(set(links))
+    with open('wheatlinks', 'w') as fl:
+        fl.write('\n'.join(links))
+    get_ipython().system(' mkdir ../images/wheat/')
+    get_ipython().system(' cd ../images/wheat/ && wget -i ../../notebooks/wheatlinks')
+
+
+# In[4]:
+
+if not os.path.exists('ricelinks'):
+    links = get_image_links('rice')
+    links = list(set(links))
+    with open('ricelinks', 'w') as fl:
+        fl.write('\n'.join(links))
+
+    get_ipython().system(' mkdir ../images/rice/')
+    get_ipython().system(' cd ../images/rice/ && wget -i ../../notebooks/ricelinks')
+
+
+# ### Creating training dataset
+
+# In[5]:
+
+wheat_paths = []
+for fl in os.listdir('../images/wheat/'):
+    path = '../images/wheat/' + fl
+    wheat_paths.append(path)
+
+
+# In[6]:
+
+rice_paths = []
+for fl in os.listdir('../images/rice/'):
+    path = '../images/rice/' + fl
+    rice_paths.append(path)
+
+
+# In[7]:
+
+wx, wy = set(), set()
+for path in wheat_paths:
+    x, y, _  = imread(path).shape
+    wx.add(x); wy.add(y)
+
+
+# In[8]:
+
+for path in rice_paths:
+    x, y, _  = imread(path).shape
+    wx.add(x); wy.add(y)
+wx = list(sorted(wx))
+wy = list(sorted(wy))
+wx[0], wy[0]
+
+
+# In[9]:
+
+images, labels = [], []
+for path in rice_paths:
+    image = imresize(imread(path), (wx[0], wy[0]))
+    images.append(image)
+    labels.append('rice')
+for path in wheat_paths:
+    image = imresize(imread(path), (wx[0], wy[0]))
+    images.append(image)
+    labels.append('wheat')
+mask = np.random.random(len(labels)) < 1
+images = np.array(images)[mask]
+labels = np.array(labels)[mask]
+
+print(len(labels))
+print(images.shape)
+
+
+# In[10]:
+
+X, y = [i.flatten() for i in images], labels
+scores = cross_val_score(RandomForestClassifier(n_jobs=-1), X, y, cv=3)
+print(scores)
+
+
+# In[11]:
+
+est = RandomForestClassifier(n_jobs=-1)
+est.fit(X, y)
+
+
+# In[12]:
+
+with open('../image_classifier.pickle', 'wb') as fl:
+    pickle.dump(est, fl)
+
+
+# In[13]:
+
+est.classes_
+
---
+++ notebooks/SeedAnalysis.py
@@ -0,0 +1,63 @@
+
+# coding: utf-8
+
+# In[39]:
+
+import pandas as pd
+import seaborn as sns
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score
+get_ipython().magic('pylab inline')
+
+
+# In[18]:
+
+df = pd.read_csv('../seeds.txt')
+df.info()
+
+
+# In[41]:
+
+X, y = df.drop('targetClass', axis=1), df.targetClass
+
+est = RandomForestClassifier(n_jobs=-1, n_estimators=50)
+grid = dict(max_depth=[2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, None],
+            max_features=[1, 2, 3, 4, 5, 6, 7],
+            min_samples_split=[2, 3, 4, 5, 6, 7, 8, 9, 10]
+           )
+gscv = GridSearchCV(est, param_grid=grid, cv=5)
+
+
+# In[42]:
+
+gscv.fit(X, y)
+
+
+# In[43]:
+
+gscv.best_params_, gscv.best_score_
+
+
+# In[45]:
+
+rf = RandomForestClassifier(n_jobs=-1, n_estimators=100, **gscv.best_params_)
+scores = cross_val_score(rf, X, y, cv=7)
+
+
+# In[47]:
+
+print(scores.mean())
+
+
+# In[48]:
+
+rf.fit(X, y)
+
+
+# In[50]:
+
+with open('../seedclassifier.pickle', 'wb') as fl:
+    pickle.dump(rf, fl)
+
