51bb47a1c1f59859d8d0efec77fb9fc00a2001ef
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-03-20 15:45:44 +0530

    basic home up

--- webserver.py
+++ webserver.py
@@ -1,8 +1,9 @@
-from bottle import Bottle, route, get, post, run
+from bottle import Bottle, route, get, post, run, template, static_file
 
-def template(name):
+def render(name, data={}):
     with open('templates/'+name) as fl:
         text = fl.read()
+    text = template(text, **data)
     return text
 
 app = Bottle()
@@ -11,8 +12,12 @@ app = Bottle()
 
 @app.route('/')
 def home():
-    html = template('home.html')
+    html = render('home.html')
     return html
 
+
 ###############################
+@app.get('/static/<path:path>')
+def callback(path):
+    return static_file(path, root='/home/arjoonn/dev/kaggle/jaipur/website/static')
 app.run(host='localhost', port=8080, debug=True)
